467fd034bbd3a35242c161f661d93082cd68b1db
illionillion <60034520+illionillion@users.noreply.github.com>
2024-07-11 02:29:18 +0000

    fix: idからクラスに修正

--- www/js/main.js
+++ www/js/main.js
@@ -9,14 +9,17 @@ window.addEventListener('load', () => {
             e.preventDefault()
         }
     })
-    document.getElementById('delete-student-form')?.addEventListener('submit', e => {
-        const data = new FormData(e.currentTarget)
-        const entries = Object.fromEntries(data.entries())
-        const isSubmit = confirm(`本当に「${entries["full-name"]}」を削除しますか？`)
-        if (!isSubmit) {
-            e.preventDefault()
-        }
-    })
+    document.querySelectorAll('.delete-student-form')?.forEach(form => {
+        form?.addEventListener('submit', e => {
+            const data = new FormData(e.currentTarget)
+            const entries = Object.fromEntries(data.entries())
+            const isSubmit = confirm(`本当に「${entries["full-name"]}」を削除しますか？`)
+            if (!isSubmit) {
+                e.preventDefault()
+            }
+        })
+    });
+    
     document.querySelectorAll('.editForm').forEach((form) => {
         form?.addEventListener('submit', e => {
             const isSubmit = confirm('本当に修正しますか？')
